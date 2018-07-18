2540224b403dfd0fa38e37c97f23f357067665b1
Venkatesan Srinivasan <vvs@tnineworks.com>
2018-07-18 23:22:36 +0530

    Added transaction processor as submodule
    Also added some semicolons :P

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ var app = express();
 
 app.get('/', function (req, res) {
     res.send('Hello World');
-})
+});
+
+app.use('/user',require('./routes/router.js'));
 
 var server = app.listen(8081, function () {
-    var host = server.address().address
-    var port = server.address().port
+    var host = server.address().address;
+    var port = server.address().port;
    
     console.log("CorMa listening at http://%s:%s", host, port)
-})
+});
--- routes/router.js
+++ routes/router.js
@@ -1,17 +1,12 @@
 const express = require('express'),
     router = express.Router();
 
-app.get('/', function (req, res) {
-    res.send('Hello World');
-})
-
-//making use of normal routes
-router.get('/john',(request,response)=>{
+router.get('/',(request,response)=>{
     response.send('Home of user');
 });
 
 router.get('/mark',(request,response)=>{
-    response.send('Home of user');
+    response.send('Home of mark');
 });
 
 //exporting thee router to other modules
