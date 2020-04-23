beb98ac42ec6cc9930faf5b45a3c808d5687d1f5
Venkatesan Srinivasan <vvs@tnineworks.com>
2020-04-24 00:34:21 +0530

    Returning a hardcoded suggestions JSON and generated from transaction identifier

--- routes/api.js
+++ routes/api.js
@@ -1,12 +1,16 @@
 const express = require('express'),
     api = express.Router();
+var fs = require('fs');
 
 api.post('/process_transactions',(request, response)=>{
-    console.log('venk010:' + request.body.mappings);
-
     response.header('Content-Type', 'text/csv');
     response.header('Content-Disposition', 'attachment; filename="mappings.csv"')
     response.send(request.body.mappings);
+});
+
+api.get('/suggestions',(request, response)=>{
+    var suggestionsJSON = JSON.parse(fs.readFileSync('./blah.json', 'utf8'));
+    response.json(suggestionsJSON);
 });
 
 module.exports = api;
